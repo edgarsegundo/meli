06dcaf5414b5fe7f93ef5d614249cd0136c1e927
Edgar Segundo <edgar.segundo@gmail.com>
2022-04-30 18:32:24 -0300

    refactoring minor changes

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { LoaderService } from '../loader/loader.service';
+import { LoaderService } from '../shared/services/loader.service';
 
 @Component({
   selector: 'app-nav',
